809a09216af6022c56bb659f287a6ea3013a7b7e
Nilesh Mali <nileshmali86@gmail.com>
2017-10-09 14:10:41 +0530

    fix: extends eslint:recommended

--- packages/eslint-config-node/index.js
+++ packages/eslint-config-node/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const rules = [
+  './rules/best-practices',
+  './rules/errors',
+  './rules/es6',
+  './rules/node',
+  './rules/style',
+  './rules/variables',
+].map(require.resolve);
+
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    './rules/best-practices',
-    './rules/errors',
-    './rules/es6',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
-  ].map(require.resolve),
+  extends: ['eslint:recommended', ...rules],
   rules: {
     strict: ['error', 'global'],
   },
